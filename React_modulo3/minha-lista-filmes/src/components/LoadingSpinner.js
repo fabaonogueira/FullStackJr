6bdc54e9887866350a0a9e48a1387f835d32b8eb
fabaonogueira <154567781+fabaonogueira@users.noreply.github.com>
2025-09-22 21:25:11 -0300

    Delete React_modulo3/minha-lista-filmes directory

--- React_modulo3/minha-lista-filmes/src/components/LoadingSpinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './LoadingSpinner.css'; // Crie um CSS para a animação
-
-const LoadingSpinner = () => {
-  return <div className="spinner"></div>;
-};
-
-export default LoadingSpinner;
--- React_modulo3/minha-lista-filmes/src/hooks/useFavorites.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { FavoritesContext } from '../context/FavoritesContext';
-
-export const useFavorites = () => {
-  return useContext(FavoritesContext);
-};
